61ae4ee30fe4f7f8b2aac922ccc30801f9babe09
yuhaocooper <tanyuhao@hotmail.com>
2020-07-07 18:48:27 +0800

    Create Calculating with Functions.js

---
+++ Codewars/JavaScript/5kyu/Calculating with Functions.js
@@ -0,0 +1,234 @@
+function zero(b) {
+  if (b==undefined){
+    return 0
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 0 + b2
+        break
+      case '-':
+        return 0 - b2
+        break
+      case '*':
+        return 0 * b2
+        break
+      case '/':
+        return Math.floor(0 / b2)
+    }
+  }
+}
+function one(b) {
+  if (b==undefined){
+    return 1
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 1 + b2
+        break
+      case '-':
+        return 1 - b2
+        break
+      case '*':
+        return 1 * b2
+        break
+      case '/':
+        return Math.floor(1 / b2)
+    }
+  }
+}
+function two(b) {
+  if (b==undefined){
+    return 2
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 2 + b2
+        break
+      case '-':
+        return 2 - b2
+        break
+      case '*':
+        return 2 * b2
+        break
+      case '/':
+        return Math.floor(2 / b2)
+    }
+  }
+}
+function three(b) {
+  if (b==undefined){
+    return 3
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 3 + b2
+        break
+      case '-':
+        return 3 - b2
+        break
+      case '*':
+        return 3 * b2
+        break
+      case '/':
+        return Math.floor(3 / b2)
+    }
+  }
+}
+function four(b) {
+  if (b==undefined){
+    return 4
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 4 + b2
+        break
+      case '-':
+        return 4 - b2
+        break
+      case '*':
+        return 4 * b2
+        break
+      case '/':
+        return Math.floor(4 / b2)
+    }
+  }
+}
+function five(b) {
+  if (b==undefined){
+    return 5
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 5 + b2
+        break
+      case '-':
+        return 5 - b2
+        break
+      case '*':
+        return 5 * b2
+        break
+      case '/':
+        return Math.floor(5 / b2)
+    }
+  }
+}
+function six(b) {
+  if (b==undefined){
+    return 6
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 6 + b2
+        break
+      case '-':
+        return 6 - b2
+        break
+      case '*':
+        return 6 * b2
+        break
+      case '/':
+        return Math.floor(6 / b2)
+    }
+  }
+}
+function seven(b) {
+  if (b==undefined){
+    return 7
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 7 + b2
+        break
+      case '-':
+        return 7 - b2
+        break
+      case '*':
+        return 7 * b2
+        break
+      case '/':
+        return Math.floor(7 / b2)
+    }
+  }
+}
+function eight(b) {
+  if (b==undefined){
+    return 8
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 8 + b2
+        break
+      case '-':
+        return 8 - b2
+        break
+      case '*':
+        return 8 * b2
+        break
+      case '/':
+        return Math.floor(8 / b2)
+    }
+  }
+}
+function nine(b) {
+  if (b==undefined){
+    return 9
+  }
+  else{
+    let b1 = b[0]
+    let b2 = parseInt(b[1])
+    switch(b1){
+      case '+':
+        return 9 + b2
+        break
+      case '-':
+        return 9 - b2
+        break
+      case '*':
+        return 9 * b2
+        break
+      case '/':
+        return Math.floor(9 / b2)
+    }
+  }
+}
+  
+
+function plus(a) {
+  return "+"+a
+}
+function minus(a) {
+  return "-"+a
+}
+function times(a) {
+  return "*"+a
+}
+function dividedBy(a) {
+  return "/"+a
+}
