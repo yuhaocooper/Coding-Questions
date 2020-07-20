74f9370f53f6ed35a8fee72425eccdf458ca98ab
yuhaocooper <tanyuhao@hotmail.com>
2020-07-20 12:23:40 +0800

    Create Factorial decomposition (working).js

---
+++ Codewars/JavaScript/5kyu/Factorial decomposition (working).js
@@ -0,0 +1,53 @@
+function decomp(n) {
+  var primes = [2]
+  var list = []
+  var temp = []
+  var result = []
+  for (var i = 2; i<n+1;i++){
+    //console.log(i)
+    //Code on all the even numbers
+    if (i % 2 ==0){
+      var x = 0;
+      var z = i;
+      while (z != 1){
+        z = z / primes[x]
+        if (z % 1 == 0){
+          temp.push(primes[x])
+        }
+        else{
+          z = z * primes[x]
+          x ++
+        }
+      }
+    }
+    //if its not a prime, keep diving by 2 till remainder is none-zero, then follow up by the next prime number
+    //odd numbers
+    else{
+      var x = 0
+      var z = i
+      while (z != 1){
+        z = z / primes[x]
+        if (z % 1 == 0){
+          temp.push(primes[x])
+        }
+        else{
+          z = z * primes[x]
+          x ++
+          if (x > primes.length - 1){
+            primes.push(Math.round(z))
+          }
+        }
+      }
+    }
+  }
+  for (var i = 0; i<primes.length; i++){
+    var count = temp.filter( num => num == primes[i]).length
+    if (count > 1){
+      result.push(primes[i] + '^' + count)
+    }
+    else{
+      result.push(primes[i])
+    }
+  }
+  return result.join(' * ')
+}
