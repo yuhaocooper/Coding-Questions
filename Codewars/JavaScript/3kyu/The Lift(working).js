bdd196f6df671d9d7e97523aeca4d64ca8996a26
yuhaocooper <tanyuhao@hotmail.com>
2020-08-09 12:13:56 +0800

    Update The Lift(working).js

--- Codewars/JavaScript/3kyu/The Lift(working).js
+++ Codewars/JavaScript/3kyu/The Lift(working).js
@@ -1,13 +1,16 @@
 var theLift = function(queues, capacity) {
   var result = []
+  let isEmpty = a => Array.isArray(a) && a.every(isEmpty);
+  var queue = []
   //function to find the next level to stop at
   //Once stop, add to result column
   //function to edit the order depending on the people and the queues
-  function up(queues,capacity,result){
-    var queue = []
+  function up(queues,capacity,result,start){
+    console.log(queues)
     for (var i = 0; i<queues.length;i++){
       if (queues[i].length != 0 || (queue.indexOf(i) != -1 && queue.length != 0) ){
-        result.push(i)
+        if(result[result.length-1] == i || (queues[i].filter(x =>x>i).length == 0 && queues[i].length >0) ) {}
+        else {result.push(i)}
         var temp = queue.slice()
         for (var x = 0; x< queue.length;x++){
           if (queue[x] == i){
@@ -16,19 +19,48 @@ var theLift = function(queues, capacity) {
         }
         queue = temp.slice()
         //Rule for boarding the lift
-        queues[i].forEach( num => {
+        queues[i].forEach( (num,index,arr) => {
           if (queue.length <= capacity && num > i){
             queue.push(num)
+            arr[index] = 'boarded'
           }
         })
+        queues[i] = queues[i].filter(x => x!= 'boarded')
         queue.sort((a,b) => a-b)
+        start = i
       }
     }
+    if(!isEmpty(queues)){down(queues,capacity,start)}
+    return result
   }
-  up (queues,capacity,result)
-  console.log(result)
-  function down(queues, capacity){
-    
+  function down(queues,capacity,start){
+    for (var i = start; i>=0;i--){
+      if (queues[i].length != 0 || (queue.indexOf(i) != -1 && queue.length != 0) ){
+        if(result[result.length-1] == i || (queues[i].filter(x =>x<i).length == 0 && queues[i].length >0) ) {}
+        else {result.push(i)}
+        var temp = queue.slice()
+        for (var x = 0; x< queue.length;x++){
+          if (queue[x] == i){
+            temp.pop()
+          }
+        }
+        queue = temp.slice()
+        //Rule for boarding the lift
+        queues[i].forEach( (num,index,arr) => {
+          if (queue.length <= capacity && num < i){
+            queue.push(num)
+            arr[index] = 'boarded'
+          }
+        })
+        queues[i] = queues[i].filter(x => x!= 'boarded')
+        queue.sort((a,b) => a-b)
+        start = i
+      }
+    }
+    return result
   }
-  
+  up(queues,capacity,result)
+  if (result[0] != 0) result.unshift(0)
+  if (result[result.length - 1] != 0) result.push(0)
+  return result
 }
