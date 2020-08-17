adb67063ecbbe2d301292632ef969d80a8369531
yuhaocooper <tanyuhao@hotmail.com>
2020-08-17 12:14:12 +0800

    Create Replace With Alphabet Position.js

---
+++ Codewars/JavaScript/6kyu/Replace With Alphabet Position.js
@@ -0,0 +1,45 @@
+function alphabetPosition(text) {
+  var arr = text.toLowerCase().split('')
+  var dict = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 9,
+    'j': 10,
+    'k': 11,
+    'l': 12,
+    'm': 13,
+    'n': 14,
+    'o': 15,
+    'p': 16,
+    'q': 17,
+    'r': 18,
+    's': 19,
+    't': 20,
+    'u': 21,
+    'v': 22,
+    'w': 23,
+    'x': 24,
+    'y': 25,
+    'z': 26
+  }
+  return arr.map(x => {
+    return dict[x] == undefined ? '' : dict[x]
+  }).filter(x => x!= '').join(' ')
+}
+
+-----------------------------------------------------------------------------------------------------------------------------
+//better solution
+
+function alphabetPosition(text) {
+  return text
+    .toUpperCase()
+    .match(/[a-z]/gi)
+    .map( (c) => c.charCodeAt() - 64)
+    .join(' ');
+}
