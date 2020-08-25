12f42a03f5f426677b91856a8d925975e207c67c
yuhaocooper <tanyuhao@hotmail.com>
2020-08-25 18:52:15 +0800

    Create Hack My Account!.js

---
+++ Codewars/JavaScript/6kyu/Hack My Account!.js
@@ -0,0 +1,9 @@
+function hack(username, password){
+  return [Object.prototype.myUsername, Object.prototype.encrypt(Object.prototype.myPassword)]
+}
+
+--------------------------------------------------------------------------------------------
+
+function hack() {
+  return [hack.myUsername, hack.encrypt(hack.myPassword)];
+}
